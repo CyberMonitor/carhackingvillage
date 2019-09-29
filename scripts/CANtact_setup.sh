c5b2571c2dad2f501f712ee96864431f8956049f
Jay Turla <shipcodez@gmail.com>
2019-09-29 17:44:02 +0800

    Create CANtact_setup.sh

---
+++ scripts/CANtact_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script enables SocketCAN with CANtact
+
+ sudo modprobe can
+ sudo modprobe vcan
+ sudo modprobe slcan
+ sudo slcand -o -c -s6 /dev/ttyACM0 can0
+ sudo ifconfig can0 up
+
